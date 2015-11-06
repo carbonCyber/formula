35582e58176c0abe2e61bea46f71e8edd1f327a1
carbonCyber <carbonCyber@gmail.com>
2015-11-06 20:17:22 +0200

    Create chop.h

---
+++ source_code/chop.h
@@ -0,0 +1,41 @@
+/*
+ *  purpose
+ *  usage
+ *  example
+ */
+
+# ifndef    FORMULA_CHOP__GUARD
+# define    FORMULA_CHOP__GUARD
+
+# include   "formula.h"
+
+namespace fml {
+
+    namespace mnl {
+
+        template <typename __C> int chop(__C &C, int beg, int end) {
+            int index = 0, replaced = 0, sub_index;
+            bool contact = 0;
+            for (auto &E : C) {
+                if (index == end) contact = 0;
+                if (!contact && index == beg) contact = 1;
+                if (contact) {
+                    sub_index = 0;
+                    for (auto &E2 : C)
+                        if (sub_index++ != replaced);
+                        else {
+                            E2 = E; ++replaced;
+                            break; }}
+                ++index; }
+            return index - replaced;
+        };
+
+    };
+
+    template <typename __C> void chop(__C &C, int beg, int end) {
+        fml::pop(C, fml::mnl::chop(C, beg, end));
+    };
+
+};
+
+# endif
