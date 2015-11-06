01ce82e748b7d6341bcb61e8611c7bbf488d1b58
carbonCyber <carbonCyber@gmail.com>
2015-11-06 21:43:12 +0200

    Update formula_count.h

--- source_code/formula_count.h
+++ source_code/formula_count.h
@@ -15,23 +15,23 @@ namespace fml {
         return cnt;
     };
 
-    template <typename __C, typename __F, typename __T, typename... ___T> int count(__C &C, const __F &F, const __T &T, const ___T&... _T) {
+    template <typename __C, typename __F, typename __T, typename... ___T> int count(const __C &C, const __F &F, const __T &T, const ___T&... _T) {
         return fml::count(C, F, T) + fml::count(C, F, _T...);
     };
 
-    template <typename __C, typename __T> int count(__C &C, const __T &T) {
+    template <typename __C, typename __T> int count(const __C &C, const __T &T) {
         return fml::count(C, [] (const __T &T1, const __T &T2) -> bool { return T1 == T2; }, T);
     };
 
-    template <typename __C, typename __T, typename... ___T> int count(__C &C, const __T &T, const ___T&... _T) {
+    template <typename __C, typename __T, typename... ___T> int count(const __C &C, const __T &T, const ___T&... _T) {
         return fml::count(C, T) + fml::count(C, [] (const __T &T1, const __T &T2) -> bool { return T1 == T2; }, _T...);
     };
 
-    template <typename __C, typename __T> int count(__C &C, const __T &T1, const __T &T2) {
+    template <typename __C, typename __T> int count(const __C &C, const __T &T1, const __T &T2) {
         return fml::count(C, T1) + fml::count(C, T2);
     };
 
-    template <typename __C, typename __T, typename... ___T> int count(__C &C, const __T &T1, const __T &T2, const ___T&... _T) {
+    template <typename __C, typename __T, typename... ___T> int count(const __C &C, const __T &T1, const __T &T2, const ___T&... _T) {
         return fml::count(C, T1, T2) + fml::count(C, _T...);
     };
 
