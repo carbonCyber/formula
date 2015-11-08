5d35196c8df4ae921f1204306b9c65708bd01ab4
carbonCyber <carbonCyber@gmail.com>
2015-11-08 15:17:40 +0200

    Update formula_chop.h

--- source_code/formula_chop.h
+++ source_code/formula_chop.h
@@ -14,20 +14,7 @@ namespace fml {
     namespace mnl {
 
         template <typename __C> int chop(__C &C, int beg, int end) {
-            int index = 0, replaced = 0, sub_index;
-            bool contact = 0;
-            for (auto &E : C) {
-                if (index == end) contact = 0;
-                if (!contact && index == beg) contact = 1;
-                if (contact) {
-                    sub_index = 0;
-                    for (auto &E2 : C)
-                        if (sub_index++ != replaced);
-                        else {
-                            E2 = E; ++replaced;
-                            break; }}
-                ++index; }
-            return index - replaced;
+            // HIDDEN //
         };
 
     };
